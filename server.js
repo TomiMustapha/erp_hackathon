9933ee100db56234a000fcfb19175f3f0bce18d1
Asim Himani <ahimani@sfu.ca>
2018-07-26 12:19:22 -0700

    Incident schema and api endpoints

--- server.js
+++ server.js
@@ -1,34 +1,52 @@
 const users = require('./routes/api/users');
-const bodyParser = require('body-parser'); 
+const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
+const app = express();
 
 const port = process.env.PORT || 8080;
-var app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));  
+// middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// db options
 var options = {
-	useNewUrlParser: true
-}
-mongoose.connect('mongodb://admin:sum4mer8@ds153851.mlab.com:53851/erp_hackathon', options, function(error) {
-	if (error) {
-		console.log(error);
-	} else {
-		console.log('db connection successful.');
-	}
+    useNewUrlParser: true
+};
+
+// db connection
+mongoose.connect(
+    process.env.mongoURI,
+    options,
+    function(error) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('db connection successful.');
+        }
+    }
+);
+
+app.all('/*', function(req, res, next) {
+    console.log(
+        'REQUEST: ' +
+            req.method +
+            ' ' +
+            req.path +
+            ' ' +
+            JSON.stringify(req.query)
+    );
+    next();
 });
 
-app.all('/*', function (req, res, next) {
-  console.log('REQUEST: ' + req.method + ' ' + req.path + ' ' + JSON.stringify(req.query));
-  next();
+// default route
+app.get('/', function(req, res) {
+    res.send('Hello World');
 });
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-});
-
+// routes
 app.use('/users', users);
+app.use('/incidents', incidents);
 
 app.listen(port, console.log('running on port', port));
