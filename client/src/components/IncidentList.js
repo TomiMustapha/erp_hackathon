f7e879464f3a83e38bdd4f9f56ca1cbc11678f5a
tomi <tomimustapha@icloud.com>
2018-07-26 16:15:50 -0700

    Navbar and Report button

---
+++ client/src/components/IncidentList.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class IncidentList extends Component {
+    render(){
+        return(
+        <div>
+            <p>IncidentList</p>
+        </div>
+        );
+    }
+}
+export default IncidentList;
