3867ae4a7a4114e1098872a04f7193df9d3459e4
Asim Himani <ahimani@sfu.ca>
2018-07-27 10:20:45 -0700

    Add back User model

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Schema
+const UserSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = User = mongoose.model('users', UserSchema);
