3e8a18a123acf26798db8f7414f729e8b8e56468
Asim Himani <ahimani@sfu.ca>
2018-07-26 19:30:48 -0700

    Create incident frontend form

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,18 @@ import './App.css';
 import AppNavbar from './components/AppNavbar';
 import IncidentList from './components/IncidentList';
 import IncidentButton from './components/IncidentButton';
+import CreateIncident from './components/CreateIncident';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <AppNavbar/>
-        <IncidentButton/>
-        <IncidentList/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <AppNavbar />
+                <CreateIncident />
+                {/* <IncidentButton/>
+        <IncidentList/> */}
+            </div>
+        );
+    }
 }
 export default App;
