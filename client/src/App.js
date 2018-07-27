b58382afd45f7757bb0d55c60c5425eab1341981
tomi <tomimustapha@icloud.com>
2018-07-26 17:16:12 -0700

    Extra files

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
       <div className="App">
         <AppNavbar/>
         <IncidentButton/>
+        <IncidentList/>
       </div>
     );
   }
--- client/src/components/IncidentList.js
+++ client/src/components/IncidentList.js
@@ -1,11 +1,27 @@
 import React, {Component} from 'react'
+import { ListGroup, ListGroupItem, Button,Container } from 'reactstrap';
 
 class IncidentList extends Component {
     render(){
         return(
-        <div>
-            <p>IncidentList</p>
-        </div>
+            <div>
+                <ListGroup>
+                    <ListGroupItem>
+                    <Container fluid>
+                <center>
+                <h1 className="display-3">Report an Incident</h1>
+                <p className="lead">
+                    <Button size="lg" color="danger">Click Here to Report an Incident</Button>
+                </p>
+                </center>
+                </Container>
+                    </ListGroupItem>
+                    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                    <ListGroupItem>Morbi leo risus</ListGroupItem>
+                    <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
+                    <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                </ListGroup>
+            </div>
         );
     }
 }
